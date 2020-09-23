5cd86596a992d709359ba3a04c9328509957666c
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-23 20:31:49 +0000

    created event to signal connection and to input a name

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+
+  conn.on('connect', (connect) => {
+    console.log('Successfully connected to the game server');
+    conn.write("Name: KNH");
+  });
+
+  // conn.on('connect', () => {
+  //   conn.write("Name: KNH");
+  // });
+
+  conn.on('data', (data) => {
+    console.log('Incoming:', data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+
+module.exports = { connect };
